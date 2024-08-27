e8c8cbdb97bcd8b63fba4476fcb0be5a98ffb296
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2024-08-27 14:23:13 -0300

    More fixes to dumb errors.

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+set -e
+
 currentDirectory=$(pwd)
 outputDirectory="/opt/Proxmox-Cloud-Image-Importer"
 
-echo "Installing Python Pip" 
-sudo apt-get install python3-pip
+if ! [ -x "$(command -v git)" ]; then
+    echo "Installing Git..." 
+    sudo apt-get install git -y
+else 
+    echo "Git is already installed. Skipping... "
+fi
+
+
+if ! [ -x "$(command -v pip3)" ]; then
+    echo "Installing Python3 Pip..." 
+    sudo apt-get install python3-pip -y
+else 
+    echo "Python3 Pip is already installed. Skipping... "
+fi
+
+echo "Downloading importer"
+git clone https://github.com/ggMartinez/Proxmox-Cloud-Image-Importer $outputDirectory && cd $outputDirectory
+
 
 echo "Installing requirements"
 pip3 install -r requirements.txt
 
-echo "Downloading importer"
-git clone https://github.com/ggMartinez/Proxmox-Cloud-Image-Importer $outputDirectory && cd $outputDirectory
 
 echo "Creating symlink"
 ln -s $outputDirectory/cloud-import.py /usr/bin/cloud-import 

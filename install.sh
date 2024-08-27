6c6f07f1aa4eec5beeac5e857857f799a3da350b
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2024-08-27 13:57:43 -0300

    Fixed install script: Forgot to go into clone directory

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+currentDirectory=$(pwd)
+outputDirectory="/opt/Proxmox-Cloud-Image-Importer"
+
 echo "Installing Python Pip" 
 sudo apt-get install python3-pip
 
@@ -7,11 +10,13 @@ echo "Installing requirements"
 pip3 install -r requirements.txt
 
 echo "Downloading importer"
-git clone https://github.com/ggMartinez/Proxmox-Cloud-Image-Importer /opt/Proxmox-Cloud-Image-Importer
+git clone https://github.com/ggMartinez/Proxmox-Cloud-Image-Importer $outputDirectory && cd $outputDirectory
 
 echo "Creating symlink"
-ln -s /opt/Proxmox-Cloud-Image-Importer/cloud-import.py /usr/bin/cloud-import 
+ln -s $outputDirectory/cloud-import.py /usr/bin/cloud-import 
 
 
 echo "Installed!! Run with 'cloud-import'"
-echo "If you want to update, run 'cd /opt/Proxmox-Cloud-Image-Importer && git pull'."
+echo "If you want to update, run \"cd $outputDirectory && git pull\"."
+
+cd $currentDirectory
